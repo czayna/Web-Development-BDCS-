abc30761852650ae21bddb8c8609543d1e5ca5f4
Yna Ortega <ynaortega10@gmail.com>
2025-11-17 12:21:56 +0800

    Updated with images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/dashboard.html"],
+    content: [
+        "./src/dashboard.html",
+        "./src/About.html",
+        "./src/Communities.html",
+        "./src/Home.html",
+        "./src/Services.html"
+    ],
     theme: {
         extend: {},
     },
